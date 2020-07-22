6d08f556d4da45ab73a8f1efbab97e296ee2328a
혜민 <lhm0710@naver.com>
2020-07-22 15:22:08 +0900

    modal, model edit

---
+++ SNUlikelion/SNUlikelion/accounts/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver 
+
+
+class Profile(models.Model):   
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    realname = models.CharField(max_length=20, blank=True)
+    major = models.CharField(max_length=20, blank=True)
+    ordinal = models.CharField(max_length=20, blank=True)
+    birthday = models.DateField(auto_now=False, auto_now_add=True)
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):  
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):  
+        instance.profile.save()
