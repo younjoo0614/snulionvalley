fb5c21f79fedcb8a001a394e2e7a4e1d78e33f61
younjoo <younjoo0614@snu.ac.kr>
2020-08-04 16:03:11 +0900

    회원가입/로그인 성공

--- booke/accounts/views.py
+++ booke/accounts/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 #from django.contrib import auth
-from .models import Profile, Follow
 from django.contrib.auth import login as django_login
 from django.contrib.auth import authenticate as django_authenticate
 from django.http import JsonResponse
+from .models import Profile, Follow
 
 
 def signup(request):
