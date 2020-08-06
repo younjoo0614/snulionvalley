348b660816bd2d0dfddd9539545991b26a9f90be
Minji Kim <minji1004y@gmail.com>
2020-08-06 14:41:45 +0900

    Change JS

--- booke/bookshelf/static/myjs.js
+++ booke/bookshelf/static/myjs.js
@@ -1,15 +1,40 @@
 $(document).ready(() => {
-    $(".more-friendlist-btn").on('click', function(event) {
+    $(".friendlist_btn").on('click', function(event) {
         $(this).toggleClass("showing-friend");
     
         if ($(this).hasClass("showing-friend")) {
             $(this).text("친구목록 닫기");
-            $(this).parent().find(".friend-list").show();
+            $(this).parent().find(".friend_list").show();
         } else {
             $(this).text("친구목록 펼치기");
-            $(this).parent().find(".friend-list").hide();
+            $(this).parent().find(".friend_list").hide();
         }
         });
         }
     )
 
+    $('.friendlist_btn').click((e) => {
+        e.preventDefault();
+        const $this = $(e.currentTarget);
+        const csrfmiddlewaretoken = $this.data('csrfmiddlewaretoken');
+    
+        $.ajax({
+            type: 'GET',
+            url: '/bookshelf/list/', 
+            data: { 
+                csrfmiddlewaretoken: csrfmiddlewaretoken,
+            },
+            dataType: "json",
+            success(res) {
+                console.log(res)
+                const follows = res.follows;
+                let friend_list=document.getElementById('friend_list')
+                
+                friend_list.innerHTML=`${follows.nickname}`;
+                },
+                
+            error(response, status, error) {
+                console.log(response, status, error);
+            }
+        })
+    })
