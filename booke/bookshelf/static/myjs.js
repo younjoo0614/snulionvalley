f0b4af3a9356c8f1c99019684fecaf6cbf27a761
Minji Kim <minji1004y@gmail.com>
2020-08-06 16:26:13 +0900

    일단커밋

--- booke/bookshelf/static/myjs.js
+++ booke/bookshelf/static/myjs.js
@@ -1,23 +1,23 @@
-$(document).ready(() => {
-    $(".friendlist_btn").on('click', function(event) {
-        $(this).toggleClass("showing-friend");
+// $(document).ready(() => {
+//     $(".friendlist_btn").on('click', function(event) {
+//         $(this).toggleClass("showing-friend");
     
-        if ($(this).hasClass("showing-friend")) {
-            $(this).text("친구목록 닫기");
-            $(this).parent().find(".friend_list").show();
-        } else {
-            $(this).text("친구목록 펼치기");
-            $(this).parent().find(".friend_list").hide();
-        }
-        });
-        }
-    )
+//         if ($(this).hasClass("showing-friend")) {
+//             $(this).text("친구목록 닫기");
+//             $(this).parent().find(".friend_list").show();
+//         } else {
+//             $(this).text("친구목록 펼치기");
+//             $(this).parent().find(".friend_list").hide();
+//         }
+//         });
+//         }
+//     )
 
     $('.friendlist_btn').click((e) => {
         e.preventDefault();
         const $this = $(e.currentTarget);
         const csrfmiddlewaretoken = $this.data('csrfmiddlewaretoken');
-    
+
         $.ajax({
             type: 'GET',
             url: '/bookshelf/list/', 
@@ -30,7 +30,20 @@ $(document).ready(() => {
                 const follows = res.follows;
                 let friend_list=document.getElementById('friend_list')
                 
-                friend_list.innerHTML=`${follows.nickname}`;
+                const friendTemplate=follows.map( profile=> `<div class="friend_list" data-id='${profile.id}'>${profile.nickname}</div>`)
+                
+                friend_list.innerHTML=friendTemplate;
+                
+                $(this).toggleClass("showing-friend");
+    
+                if ($(this).hasClass("showing-friend")) {
+                    $(this).text("친구목록 닫기");
+                    $(this).parent().find(".friend_list").show();
+                } else {
+                    $(this).text("친구목록 펼치기");
+                    $(this).parent().find(".friend_list").hide();
+                }
+
                 },
                 
             error(response, status, error) {
